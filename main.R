3726d33e3cf8728d28a89f515aecff1be298f6d0
MattjCamp <mjcampbell74@gmail.com>
2016-04-26 13:36:57 -0400

    Moved R Project files

---
+++ main.R
@@ -0,0 +1,8 @@
+
+
+dirs <- list()
+dirs$r.lib <- sprintf("%sR-Code-Library/",
+                      dirs$proj)
+dirs$data.sources <- sprintf("%sdata-sources/",
+                             getwd())
+
